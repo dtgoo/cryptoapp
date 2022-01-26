6f4caa1e0e697db3f132a903b16771a7d321b9fd
Jesse Perez <jessep1130@gmail.com>
2022-01-25 21:36:22 -0600

    changed app name to cryptocrunch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layour, Typograpy, Space } from 'antd';
+
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="app">
+        <div className="navbar">
+
+        </div>
+        <div className="main">
+
+        </div>
+        <div className="footer">
+            
+        </div>
     </div>
   )
 }
