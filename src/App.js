47cdc8729308afb7d624d99b6df393bb9c80057c
Jesse Perez <jessep1130@gmail.com>
2022-01-30 23:19:58 -0600

    importing components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typograpy, Space } from 'antd';
 
-import { Navbar, } from './components';
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News} from './components';
 import './App.css'
 
 const App = () => {
