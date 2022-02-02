5293597d718cbd622f89a0b7c27496c7e30b676b
Jesse Perez <jessep1130@gmail.com>
2022-02-01 21:52:51 -0600

    working on homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,7 +10,21 @@ const HomePage = () => {
       <>
       <Title level={2} className ='heading'>Global Crypto Stats</Title>
       <Row>
-        
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value='53'/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value='5'/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value='5'/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24 Hour Volume" value='5'/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value='5'/>
+        </Col>
       </Row>
       </>
   )
