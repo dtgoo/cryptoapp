ac911f3485114acc79207844eed79f0089a3302f
Jesse Perez <jessep1130@gmail.com>
2022-02-07 22:23:44 -0600

    using millify to make integers look better

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'; 
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
@@ -9,27 +9,27 @@ const { Title } = Typography;
 
 const HomePage = () => {
   const { data, isFetching } = useGetCryptosQuery();
-  console.log(data);
+  const globalStats = data?.data?.stats;
 
-  if(isFetching) return 'Loading...'
+  if(isFetching) return 'Loading...';
   return (
       <>
       <Title level={2} className ='heading'>Global Crypto Stats</Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value='53'/>
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total}/>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value='5'/>
+          <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value='5'/>
+          <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/>
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24 Hour Volume" value='5'/>
+          <Statistic title="Total 24 Hour Volume" value={millify(globalStats.total24hVolume)}/>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value='5'/>
+          <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/>
         </Col>
       </Row>
       </>
