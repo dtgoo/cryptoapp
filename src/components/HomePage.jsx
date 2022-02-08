29773061092874b1b6c76ab5df569430ad2e4e37
Jesse Perez <jessep1130@gmail.com>
2022-02-07 23:42:54 -0600

    finally got antd cards rendering on page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -42,7 +42,7 @@ const HomePage = () => {
           </Link>
         </Title>
       </div>
-      <Cryptocurrencies/>
+      <Cryptocurrencies simplified/>
       <div className='home-heading-container'>
         <Title level={2} className='home-title'>Latest Crypto News</Title>
         <Title level={3} className='show-more'>
