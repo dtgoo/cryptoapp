7ef165f2025b1d11ee04437888c09e4d820be47c
Jesse Perez <jessep1130@gmail.com>
2022-02-07 22:45:08 -0600

    rendering base components on page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,6 +4,7 @@ import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom'; 
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
 
 const { Title } = Typography;
 
@@ -12,6 +13,7 @@ const HomePage = () => {
   const globalStats = data?.data?.stats;
 
   if(isFetching) return 'Loading...';
+
   return (
       <>
       <Title level={2} className ='heading'>Global Crypto Stats</Title>
@@ -32,6 +34,24 @@ const HomePage = () => {
           <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/>
         </Col>
       </Row>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
+        <Title level={3} className='show-more'>
+          <Link to='/cryptocurrencies'>
+            Show more
+          </Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified/>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>Latest Crypto News</Title>
+        <Title level={3} className='show-more'>
+          <Link to='/news'>
+            Show more
+          </Link>
+        </Title>
+      </div>
+      <News simplified/>
       </>
   )
 }
