1caf73d430ff2da8bd95f603b9b540dc1ea187f2
Jesse Perez <jessep1130@gmail.com>
2022-02-16 23:18:46 -0600

    working on conditional rendering

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -8,7 +8,8 @@ const { Option } = Select;
 
 const News = ({ simplified }) => {
   const {data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count : simplified? 10: 100});
-  console.log(cryptoNews)
+
+  if (!cryptoNews?.value) return 'Loading...';
   return (
     <div>
       News
