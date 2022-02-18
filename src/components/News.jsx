ef85bb6932583c567dcb9d6a778e1c53f14aeee7
Jesse Perez <jessep1130@gmail.com>
2022-02-17 22:14:38 -0600

    now able to see and open cards for news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Select, Typography, Row, Column, Avatar, Card } from 'antd';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
@@ -7,13 +7,26 @@ const { Text, Title } = Typography;
 const { Option } = Select; 
 
 const News = ({ simplified }) => {
-  const {data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count : simplified? 10: 100});
+  const {data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count : simplified? 6: 12});
 
   if (!cryptoNews?.value) return 'Loading...';
   
   return (
     <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className='news-card'>
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className= "news-image-container">
+                <Title className='news-title' level={4}>
+                  {news.name}
+                </Title>
+              </div>
+            </a>
 
+          </Card>
+        </Col>
+      ))}
     </Row>
   )
 }
