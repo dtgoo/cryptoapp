1b175611850e10951f4cb24d338cb96798c1256f
Jesse Perez <jessep1130@gmail.com>
2022-03-01 20:54:14 -0600

    will now add crypto choices for new

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 import demoImage from "../images/doge_small.png"
 
 const { Text, Title } = Typography;
@@ -9,8 +10,8 @@ const { Option } = Select;
 
 const News = ({ simplified }) => {
   const[newsCategory, setNewsCategory] = useState('Cryptocurrency');
-  
-  const {data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count : simplified ? 6 : 12});
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count : simplified ? 6 : 12});
+  const { data } = useGetCryptosQuery(100);
 
   if (!cryptoNews?.value) return 'Loading...';
   
@@ -26,6 +27,7 @@ const News = ({ simplified }) => {
             onChange={(value)=> console.log(value)}
             filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
             >
+              <Option value='Cryptocurrency'> Cryptocurrency </Option>
           </Select>
         </Col>
       )}
